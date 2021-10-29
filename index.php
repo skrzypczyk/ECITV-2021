2b2d5d42914696357e058ba601ffcae861306e2c
Yves skrzypczyk <yvesskrzypczyk@MacBook-Pro-de-Yves.local>
2021-10-29 08:33:41 +0200

    debut de cours

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Ceci est un titre</title>
+	<meta name="description" content="ceci est la description de ma page">
+</head>
+<body>
+	<h1>
+		Nous sommes le <?php echo date("d/m/Y");?>
+		
+	</h1>
+ 	<p>
+ 		Lorem &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <b>ipsum</b> <strong>dolor</strong> <i>sit</i> amet, <u>consectetur</u> <s>adipisicing</s> elit, sed do eiusmod
+ 		tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+ 		quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+ 		consequat.<br> Duis aute irure dolor in reprehenderit in voluptate velit esse
+ 		cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+ 		proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+ 	</p>
+</body>
+</html>
